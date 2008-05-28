7f0a6849dd58d91ac477a3bd95c2a1d679ee5cc8
reachsrk <reachsrk@d7bd5baf-2c48-0410-9bef-f3bac81b324d>
2008-05-28 20:38:17 +0000

    cleaning up to help ppl to use it

---
+++ _compile.sh
@@ -0,0 +1,28 @@
+#WARNING::Yet to be implemented!!!!!
+
+
+ERL_CALL=$ERL_HOME/lib/erl_interface/bin/i686-pc-linux-gnu/erl_call
+#
+$ERL_CALL -sname perferl -s
+#$ERL_CALL -sname perferl -a 'code add_path ["/home/srk/dev/erlang/perferl/lib/yaws/ebin"]'
+$ERL_CALL -sname perferl -a 'code add_path ["/home/srk/dev/erlang/perferl/lib/erlyweb-0.6.2/ebin"]'
+#
+#$ERL_CALL -sname perferl -a 'yaws start_embedded ["/home/srk/dev/erlang/perferl/www",
+#		[{servername, "srk"},
+#  		 {listen, {0,0,0,0}},
+#		 {port, 9080},
+#		 {appmods, [{"/perferl", erlyweb}]},
+#		 {opaque, [{"appname","perferl"}]}],
+#        [{auth_log, true},
+#         {logdir, "/home/srk/dev/erlang/perferl/log"},
+#		 {copy_errlog, false}]]'
+$ERL_CALL -sname perferl -a 'erlyweb compile ["/home/srk/dev/erlang/perferl"]'
+#
+#$ERL_CALL -sname worker -s
+#$ERL_CALL -sname worker -a 'code add_path ["/home/srk/dev/erlang/perferl/ebin"]'
+#$ERL_CALL -sname worker -a 'inets start'
+#
+#
+#echo "Started perferl"
+$ERL_CALL -sname perferl -q
+
